cdda140ade718f1f5a08e821c37fa22207f89d93
Pelumi Ale <alepelumi@gmail.com>
2026-08-02 21:39:12 +0100

    l

--- src/academic-sessions/dto/create-academic-session.dto.ts
+++ src/academic-sessions/dto/create-academic-session.dto.ts
@@ -1,11 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Semester } from '@prisma/client';
+import { IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateAcademicSessionDto {
   @ApiProperty({ example: '2025/2026' })
   @IsString()
   @IsNotEmpty()
   name!: string;
+
+  @ApiProperty({ enum: Semester, example: Semester.FIRST })
+  @IsEnum(Semester)
+  semester!: Semester;
 
   @ApiProperty({ example: true, required: false })
   @IsBoolean()
